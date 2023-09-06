3198306a2cea1a9422668c6423298ce3e39cf495
marcprux <mwp1@cornell.edu>
2023-09-05 20:19:06 -0400

    Remove Kt targets

--- Package.swift
+++ Package.swift
@@ -7,13 +7,9 @@ let package = Package(
     platforms: [.macOS("13"), .iOS("16")],
     products: [
         .library(name: "AppUI", targets: ["AppUI"]),
-        .library(name: "AppUIKt", targets: ["AppUIKt"]),
     ],
     dependencies: [
         .package(url: "https://source.skip.tools/skip.git", from: "0.6.12"),
-        .package(url: "https://source.skip.tools/skip-unit.git", from: "0.2.0"),
-        .package(url: "https://source.skip.tools/skip-lib.git", from: "0.3.0"),
-        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.1.0"),
         .package(url: "https://source.skip.tools/skip-ui.git", from: "0.1.1"),
     ],
     targets: [
@@ -22,27 +18,10 @@ let package = Package(
 
         // The Swift side of the app's user interface (SwiftUI)
         .target(name: "AppUI",
-            dependencies: [],
+            dependencies: [.product(name: "SkipUI", package: "skip-ui", condition: .when(platforms: [.macOS]))],
             resources: [.process("Resources")],
-            plugins: [.plugin(name: "skippy", package: "skip")]),
+            plugins: [.plugin(name: "skipstone", package: "skip")]),
         .testTarget(name: "AppUITests", dependencies: ["AppUI"],
-            plugins: [.plugin(name: "skippy", package: "skip")]),
-
-        // The Kotlin side of the app's user interface (Jetpack Compose)
-        .target(name: "AppUIKt",
-            dependencies: [
-                .product(name: "SkipUnitKt", package: "skip-unit"),
-                .product(name: "SkipLibKt", package: "skip-lib"),
-                .product(name: "SkipFoundationKt", package: "skip-foundation"),
-                .product(name: "SkipUIKt", package: "skip-ui"),
-            ],
-            resources: [.process("Skip")],
-            plugins: [.plugin(name: "transpile", package: "skip")]),
-        .testTarget(name: "AppUIKtTests",
-            dependencies: [
-                "AppUIKt",
-                .product(name: "SkipUnit", package: "skip-unit"),
-            ], resources: [.process("Skip")],
-            plugins: [.plugin(name: "transpile", package: "skip")]),
+            plugins: [.plugin(name: "skipstone", package: "skip")]),
     ]
 )
---
+++ Tests/AppUITests/XCSkipTests.swift
@@ -0,0 +1,15 @@
+#if canImport(SkipUnit)
+import SkipUnit
+
+/// This test case will run the transpiled tests for the Skip module.
+@available(macOS 13, macCatalyst 16, *)
+final class XCSkipTests: XCTestCase, XCGradleHarness {
+    public func testSkipModule() async throws {
+        #if DEBUG
+        try await gradle(actions: ["testDebug"])
+        #else
+        try await gradle(actions: ["testRelease"])
+        #endif
+    }
+}
+#endif
