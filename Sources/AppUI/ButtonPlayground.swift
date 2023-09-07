ab15ce9d0d2a2a1e5bdb5a7fe245f11fce47c6f0
marcprux <mwp1@cornell.edu>
2023-09-07 14:36:52 -0400

    Fix Button initializers

--- Sources/AppUI/ButtonPlayground.swift
+++ Sources/AppUI/ButtonPlayground.swift
@@ -4,6 +4,7 @@
 // under the terms of the GNU Lesser General Public License 3.0
 // as published by the Free Software Foundation https://fsf.org
 import SwiftUI
+import Foundation
 
 struct ButtonPlayground: View {
     @State var tapCount = 0
