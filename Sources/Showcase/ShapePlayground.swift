859e46ea1d8c53b3aae9ef46fce1d52a9cd16b8a
Abe White <abewhite@mac.com>
2023-11-06 19:08:34 -0600

    Snapshot Shape playground

--- Sources/Showcase/ShapePlayground.swift
+++ Sources/Showcase/ShapePlayground.swift
@@ -9,10 +9,9 @@ struct ShapePlayground: View {
     var body: some View {
         ScrollView {
             VStack(spacing: 16.0) {
-                //~~~
-                #if !SKIP
+                Text("Circle").font(.title).bold()
                 HStack {
-                    Text("Circle.fill()")
+                    Text("fill()")
                     Spacer()
                     ZStack {
                         Circle()
@@ -22,7 +21,100 @@ struct ShapePlayground: View {
                     .border(.blue)
                 }
                 HStack {
-                    Text("Circle.offset(20.0, 20.0).fill()")
+                    Text("fill()")
+                    Spacer()
+                    ZStack {
+                        Circle()
+                            .fill()
+                    }
+                    .frame(width: 50.0, height: 100.0)
+                    .border(.blue)
+                }
+                HStack {
+                    Text("fill(.red)")
+                    Spacer()
+                    ZStack {
+                        Circle()
+                            .fill(.red)
+                    }
+                    .frame(width: 100.0, height: 100.0)
+                    .border(.blue)
+                }
+                HStack {
+                    Text("fill(.red.gradient)")
+                    Spacer()
+                    ZStack {
+                        Circle()
+                            .fill(.red.gradient)
+                    }
+                    .frame(width: 100.0, height: 100.0)
+                    .border(.blue)
+                }
+                HStack {
+                    Text("stroke()")
+                    Spacer()
+                    ZStack {
+                        Circle()
+                            .stroke()
+                    }
+                    .frame(width: 100.0, height: 100.0)
+                    .border(.blue)
+                }
+                HStack {
+                    Text("stroke(.red, lineWidth: 10)")
+                    Spacer()
+                    ZStack {
+                        Circle()
+                            .stroke(.red, lineWidth: 10.0)
+                    }
+                    .frame(width: 100.0, height: 100.0)
+                    .border(.blue)
+                }
+                HStack {
+                    Text("stroke(.red.gradient, lineWidth: 10)")
+                    Spacer()
+                    ZStack {
+                        Circle()
+                            .stroke(.red.gradient, style: StrokeStyle(lineWidth: 10.0))
+                    }
+                    .frame(width: 100.0, height: 100.0)
+                    .border(.blue)
+                }
+                HStack {
+                    Text("stroke(.red,\n    style: StrokeStyle(\n    lineWidth: 10.0,\n      dash: [10]))")
+                    Spacer()
+                    ZStack {
+                        Circle()
+                            .stroke(.red, style: StrokeStyle(lineWidth: 10.0, dash: [10.0]))
+                    }
+                    .frame(width: 100.0, height: 100.0)
+                    .border(.blue)
+                }
+                HStack {
+                    Text("strokeBorder(.red, lineWidth: 10)")
+                    Spacer()
+                    ZStack {
+                        Circle()
+                            .strokeBorder(.red, lineWidth: 10.0)
+                    }
+                    .frame(width: 100.0, height: 100.0)
+                    .border(.blue)
+                }
+                HStack {
+                    Text("fill(.red)\n  .stroke(.green, lineWidth: 10)")
+                    Spacer()
+                    ZStack {
+                        if #available(iOS 17, *) {
+                            Circle()
+                                .fill(.red)
+                                .stroke(.green, lineWidth: 10.0)
+                        }
+                    }
+                    .frame(width: 100.0, height: 100.0)
+                    .border(.blue)
+                }
+                HStack {
+                    Text("offset(20, 20).fill()")
                     Spacer()
                     ZStack {
                         Circle()
@@ -32,7 +124,6 @@ struct ShapePlayground: View {
                     .frame(width: 100.0, height: 100.0)
                     .border(.blue)
                 }
-                #endif
             }.padding()
         }
     }
