bae83f02c095222138a2c6d59403b9375e9254fc
Abe White <abewhite@mac.com>
2023-11-16 11:22:30 -0600

    More gradient playgrounding

--- Sources/Showcase/GradientPlayground.swift
+++ Sources/Showcase/GradientPlayground.swift
@@ -12,7 +12,27 @@ struct GradientPlayground: View {
                 HStack {
                     Text(".red.gradient")
                     Spacer()
-                    Text("Text").font(.largeTitle).foregroundStyle(.red.gradient)
+                    Rectangle()
+                        .fill(.red.gradient)
+                        .frame(width: 100.0, height: 100.0)
+                }
+                HStack {
+                    Text("EllipitcalGradient")
+                    Spacer()
+                    EllipticalGradient(colors: [.red, .blue], center: UnitPoint(x: 0.5, y: 0.5), startRadiusFraction: 0.25)
+                        .frame(width: 50.0, height: 100.0)
+                }
+                HStack {
+                    Text("LinearGradient")
+                    Spacer()
+                    LinearGradient(colors: [.red, .blue], startPoint: UnitPoint(x: 0.0, y: 0.0), endPoint: UnitPoint(x: 1.0, y: 1.0))
+                        .frame(width: 100.0, height: 100.0)
+                }
+                HStack {
+                    Text("RadialGradient")
+                    Spacer()
+                    RadialGradient(colors: [.red, .blue], center: UnitPoint(x: 0.5, y: 0.5), startRadius: 25.0, endRadius: 50.0)
+                        .frame(width: 100.0, height: 100.0)
                 }
             }
             .padding()
