8687b3699918067943182c0a005843ef7dc873e2
Abe White <abewhite@mac.com>
2023-11-19 22:15:49 -0600

    Bump required SkipUI version

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://source.skip.tools/skip.git", from: "0.7.28"),
-        .package(url: "https://source.skip.tools/skip-ui.git", from: "0.3.16")
+        .package(url: "https://source.skip.tools/skip-ui.git", from: "0.3.19")
     ],
     targets: [
         .target(name: "Showcase", dependencies: (zero ? [] : [.product(name: "SkipUI", package: "skip-ui")]), resources: [.process("Resources")], plugins: skipstone),
