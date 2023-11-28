bcbb8869cbbd61197a6460ee6e8882a888698141
Abe White <abewhite@mac.com>
2023-11-28 07:47:34 -0600

    Bump required SkipUI version

--- Sources/Showcase/VideoPlayerPlayground.swift
+++ Sources/Showcase/VideoPlayerPlayground.swift
@@ -4,10 +4,9 @@
 // under the terms of the GNU Lesser General Public License 3.0
 // as published by the Free Software Foundation https://fsf.org
 
-import SwiftUI
 import AVKit
+import SwiftUI
 
-/// Demo of a video player
 struct VideoPlayerPlayground: View {
     @State var player = AVPlayer(playerItem: AVPlayerItem(url: URL(string: "https://skip.tools/assets/introduction.mov")!))
     @State var isPlaying: Bool = false
@@ -16,16 +15,13 @@ struct VideoPlayerPlayground: View {
         VStack {
             Button {
                 isPlaying ? player.pause() : player.play()
-                //isPlaying.toggle()
                 isPlaying = !isPlaying
                 player.seek(to: .zero)
             } label: {
                 Image(systemName: isPlaying ? "stop" : "play")
                     .padding()
             }
-
             VideoPlayer(player: player)
-                //.frame(width: 320.0, height: 180.0, alignment: .center)
         }
     }
 }
