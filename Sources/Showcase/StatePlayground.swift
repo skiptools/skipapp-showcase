7371f73c39d0d62ce0618f309a9097f9c22293d2
Abe White <abewhite@mac.com>
2024-01-01 12:28:54 -0600

    Additional State exercising

--- Sources/Showcase/StatePlayground.swift
+++ Sources/Showcase/StatePlayground.swift
@@ -9,8 +9,23 @@ class TapCountObservable: ObservableObject {
     @Published var tapCount = 0
 }
 
-struct TapCountStruct {
+struct TapCountStruct : Identifiable {
+    var id = 0
     var tapCount = 0
+}
+
+class TapCountRepository: ObservableObject {
+    @Published var items: [TapCountStruct] = []
+
+    func add() {
+        items.append(TapCountStruct(id: items.count))
+    }
+
+    func increment() {
+        if !items.isEmpty {
+            items[items.count - 1].tapCount += 1
+        }
+    }
 }
 
 struct StatePlayground: View {
@@ -18,10 +33,14 @@ struct StatePlayground: View {
     @State var hasStateTapped: Bool? // Test optional vars
     @StateObject var tapCountObservable = TapCountObservable()
     @State var tapCountStruct = TapCountStruct()
+    @StateObject var tapCountRepository = TapCountRepository() // Test ForEach observable
 
     var body: some View {
-        ScrollView {
-            VStack(spacing: 16.0) {
+        List {
+            Section {
+                NavigationLink("Push another", value: PlaygroundType.state)
+            }
+            Section {
                 if hasStateTapped == true {
                     Text("State tap count: \(tapCount)")
                 } else {
@@ -32,37 +51,37 @@ struct StatePlayground: View {
                     hasStateTapped = true
                 }
                 StatePlaygroundBindingView(tapCount: $tapCount)
-
-                Divider()
-
+            }
+            Section {
                 Text("Observable tap count: \(tapCountObservable.tapCount)")
                 Button("Observable") {
                     tapCountObservable.tapCount += 1
                 }
                 StatePlaygroundBindingView(tapCount: $tapCountObservable.tapCount)
-
-                Divider()
-
+            }
+            Section {
                 Text("Struct tap count: \(tapCountStruct.tapCount)")
                 Button("Struct") {
                     tapCountStruct.tapCount += 1
                 }
                 StatePlaygroundBindingView(tapCount: $tapCountStruct.tapCount)
-
-                Divider()
-                
-                NavigationLink("Push another", value: PlaygroundType.state)
             }
-            .padding()
-            .onChange(of: tapCount) {
-                logger.log("onChange(of: tapCount): \($0)")
+            Section {
+                ForEach(tapCountRepository.items) { item in
+                    Text("Repository item tap count: \(item.tapCount)")
+                }
+                Button("Add item") { tapCountRepository.add() }
+                Button("Increment last") { tapCountRepository.increment() }
             }
-            .onChange(of: tapCountObservable.tapCount) {
-                logger.log("onChange(of: tapCountObservable.tapCount): \($0)")
-            }
-            .onChange(of: tapCountStruct.tapCount) {
-                logger.log("onChange(of: tapCountStruct.tapCount): \($0)")
-            }
+        }
+        .onChange(of: tapCount) {
+            logger.log("onChange(of: tapCount): \($0)")
+        }
+        .onChange(of: tapCountObservable.tapCount) {
+            logger.log("onChange(of: tapCountObservable.tapCount): \($0)")
+        }
+        .onChange(of: tapCountStruct.tapCount) {
+            logger.log("onChange(of: tapCountStruct.tapCount): \($0)")
         }
         .toolbar {
             PlaygroundSourceLink(file: "StatePlayground.swift")
