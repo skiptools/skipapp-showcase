cb6f6f7dc8ff3a5eeba390b7cb48c5a9bba5302a
Abe White <abewhite@mac.com>
2024-07-03 11:30:32 -0500

    Additional Table exercising

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         .library(name: "ShowcaseApp", type: .dynamic, targets: ["Showcase"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "0.9.2"),
-        .package(url: "https://source.skip.tools/skip-ui.git", from: "0.10.0"),
+        .package(url: "https://source.skip.tools/skip.git", from: "0.9.4"),
+        .package(url: "https://source.skip.tools/skip-ui.git", from: "0.10.1"),
         .package(url: "https://source.skip.tools/skip-av.git", from: "0.0.3")
     ],
     targets: [
--- Sources/Showcase/TablePlayground.swift
+++ Sources/Showcase/TablePlayground.swift
@@ -6,12 +6,24 @@
 import SwiftUI
 
 enum TablePlaygroundType: String, CaseIterable {
-    case evenColumns
+    case defaultColumns
+    case fixedWidthColumns
+    case rangeWidthColumns
+    case selection
+    case selectionSet
 
     var title: String {
         switch self {
-        case .evenColumns:
-            return "Even Columns"
+        case .defaultColumns:
+            return "Default Columns"
+        case .fixedWidthColumns:
+            return "Fixed Width Columns"
+        case .rangeWidthColumns:
+            return "Range Width Columns"
+        case .selection:
+            return "Selection"
+        case .selectionSet:
+            return "Selection Set"
         }
     }
 }
@@ -26,8 +38,20 @@ struct TablePlayground: View {
         }
         .navigationDestination(for: TablePlaygroundType.self) {
             switch $0 {
-            case .evenColumns:
-                EvenColumnsTablePlayground()
+            case .defaultColumns:
+                DefaultColumnsTablePlayground()
+                    .navigationTitle($0.title)
+            case .fixedWidthColumns:
+                FixedWidthColumnsTablePlayground()
+                    .navigationTitle($0.title)
+            case .rangeWidthColumns:
+                RangeWidthColumnsTablePlayground()
+                    .navigationTitle($0.title)
+            case .selection:
+                SelectionTablePlayground()
+                    .navigationTitle($0.title)
+            case .selectionSet:
+                SelectionSetTablePlayground()
                     .navigationTitle($0.title)
             }
         }
@@ -38,22 +62,99 @@ private struct TableData: Identifiable {
     let id = UUID()
     let name: String
     let value: Int
-}
-
-private struct EvenColumnsTablePlayground: View {
-    @State var data = Self.initialData
-
-    var body: some View {
-        Table(data) {
-            TableColumn("Name", value: \.name)
-            TableColumn("Value", content: { data in
-                Text("\(data.value)")
-            })
-        }
-    }
 
     static var initialData: [TableData] {
         return (1...20).map { TableData(name: "Item \($0)", value: $0) }
     }
 }
 
+private struct DefaultColumnsTablePlayground: View {
+    @State var data = TableData.initialData
+
+    var body: some View {
+        Table(data) {
+            TableColumn("UUID", content: { data in
+                // SKIP NOWARN
+                Text(String(data.id.uuidString.prefix(8)))
+            })
+            TableColumn("Name", value: \.name)
+            TableColumn("Value", content: { data in
+                Text("\(data.value)")
+            })
+        }
+    }
+}
+
+private struct FixedWidthColumnsTablePlayground: View {
+    @State var data = TableData.initialData
+
+    var body: some View {
+        Table(data) {
+            TableColumn("UUID", content: { data in
+                // SKIP NOWARN
+                Text(String(data.id.uuidString.prefix(8)))
+            })
+            .width(100)
+            TableColumn("Name", value: \.name)
+                .width(100)
+            TableColumn("Value", content: { data in
+                Text("\(data.value)")
+            })
+        }
+    }
+}
+
+private struct RangeWidthColumnsTablePlayground: View {
+    @State var data = TableData.initialData
+
+    var body: some View {
+        Table(data) {
+            TableColumn("UUID", content: { data in
+                // SKIP NOWARN
+                Text(String(data.id.uuidString.prefix(8)))
+            })
+            .width(min: 100, max: 200)
+            TableColumn("Name", value: \.name)
+                .width(ideal: 100)
+            TableColumn("Value", content: { data in
+                Text("\(data.value)")
+            })
+        }
+    }
+}
+
+private struct SelectionTablePlayground: View {
+    @State var data = TableData.initialData
+    @State var selection: UUID?
+
+    var body: some View {
+        Table(data, selection: $selection) {
+            TableColumn("UUID", content: { data in
+                // SKIP NOWARN
+                Text(String(data.id.uuidString.prefix(8)))
+            })
+            TableColumn("Name", value: \.name)
+            TableColumn("Value", content: { data in
+                Text("\(data.value)")
+            })
+        }
+    }
+}
+
+private struct SelectionSetTablePlayground: View {
+    @State var data = TableData.initialData
+    @State var selection: Set<UUID> = []
+
+    var body: some View {
+        Table(data, selection: $selection) {
+            TableColumn("UUID", content: { data in
+                // SKIP NOWARN
+                Text(String(data.id.uuidString.prefix(8)))
+            })
+            TableColumn("Name", value: \.name)
+            TableColumn("Value", content: { data in
+                Text("\(data.value)")
+            })
+        }
+    }
+}
