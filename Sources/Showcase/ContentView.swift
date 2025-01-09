73a681e6c0e466674594adc303e11f1a6960df6b
Marc Prud'hommeaux <marc@skip.tools>
2025-01-09 01:47:35 -0500

    Add VideoPlayer looping sample, MapPlayground sample, and more localization examples

--- Sources/Showcase/ContentView.swift
+++ Sources/Showcase/ContentView.swift
@@ -5,18 +5,37 @@
 // as published by the Free Software Foundation https://fsf.org
 import SwiftUI
 
+enum ContentTab: String, Hashable {
+    case about, showcase, settings
+}
+
 struct ContentView: View {
+    @AppStorage("tab") var tab = ContentTab.about
+    @AppStorage("appearance") var appearance = ""
+
     var body: some View {
-        TabView {
+        TabView(selection: $tab) {
             AboutView()
+                .tag(ContentTab.about)
                 .tabItem {
                     Label("About", systemImage: "info.circle")
                 }
             PlaygroundNavigationView()
+                .tag(ContentTab.showcase)
                 .tabItem {
-                    Label("Showcase", systemImage: "list.bullet")
+                    Label {
+                        Text("Showcase")
+                    } icon: {
+                        Image("widgets", bundle: .module, label: Text("Showcase Icon"))
+                    }
+                }
+            SettingsView(appearance: $appearance)
+                .tag(ContentTab.settings)
+                .tabItem {
+                    Label("Settings", systemImage: "gearshape.fill")
                 }
         }
+        .preferredColorScheme(appearance == "dark" ? .dark : appearance == "light" ? .light : nil)
     }
 }
 
