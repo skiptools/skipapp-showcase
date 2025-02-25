e0e56b3a341c832678edd1b5a0922ec617897a30
Marc Prud'hommeaux <marc@skip.tools>
2025-02-25 14:53:43 -0500

    Rename Main.swift in keeping with new project template

--- Android/app/src/main/kotlin/Main.kt
+++ Android/app/src/main/kotlin/Main.kt
@@ -44,6 +44,8 @@ open class MainActivity: AppCompatActivity {
 
     override fun onCreate(savedInstanceState: android.os.Bundle?) {
         super.onCreate(savedInstanceState)
+        logger.info("starting activity")
+        UIApplication.launch(this)
         enableEdgeToEdge()
 
         setContent {
