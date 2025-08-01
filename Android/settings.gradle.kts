44883b3f22518f5f2000d413c5baaa3e685b1b69
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-08-01 14:29:58 +0200

    Update Android project settings for Gradle 9.0

--- Android/settings.gradle.kts
+++ Android/settings.gradle.kts
@@ -5,11 +5,16 @@ pluginManagement {
 
     // overriding outputs for an Android IDE can be done by un-commenting and setting the Xcode path:
     //System.setProperty("BUILT_PRODUCTS_DIR", "${System.getProperty("user.home")}/Library/Developer/Xcode/DerivedData/MySkipProject-HASH/Build/Products/Debug-iphonesimulator")
-    @Suppress("DEPRECATION")
-    exec {
+
+    val skipPluginResult = providers.exec {
         commandLine("/bin/sh", "-c", "skip plugin --prebuild --package-path '${settings.rootDir.parent}' --plugin-ref '${pluginPath.absolutePath}'")
         environment("PATH", "${System.getenv("PATH")}:/opt/homebrew/bin")
     }
+    val skipPluginOutput = skipPluginResult.standardOutput.asText.get()
+    print(skipPluginOutput)
+    val skipPluginError = skipPluginResult.standardError.asText.get()
+    print(skipPluginError)
+
     includeBuild(pluginPath.readText()) {
         name = "skip-plugins"
     }
