70bec9b8cd8be4260f516f3bc38f7a98176d793a
fhasse95 <49185957+fhasse95@users.noreply.github.com>
2025-12-15 04:32:09 +0100

    Bugfix: Status bar does not update when the appearance changes on Android (#50)
    * Bugfix: Status bar not updating on appearance change
    
    * React to theme changes
    
    * Update Main.kt

--- Android/app/src/main/kotlin/Main.kt
+++ Android/app/src/main/kotlin/Main.kt
@@ -7,18 +7,24 @@ import skip.ui.*
 
 import android.Manifest
 import android.app.Application
-import androidx.activity.enableEdgeToEdge
+import android.graphics.Color as AndroidColor
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.SystemBarStyle
+import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.saveable.rememberSaveableStateHolder
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.luminance
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.material3.MaterialTheme
 import androidx.core.app.ActivityCompat
 
 internal val logger: SkipLogger = SkipLogger(subsystem = "showcase.app", category = "ShowcaseApp")
@@ -27,13 +33,13 @@ internal val logger: SkipLogger = SkipLogger(subsystem = "showcase.app", categor
 open class AndroidAppMain: Application {
     constructor() {
     }
-
+    
     override fun onCreate() {
         super.onCreate()
         logger.info("starting app")
         ProcessInfo.launch(applicationContext)
     }
-
+    
     companion object {
     }
 }
@@ -42,13 +48,13 @@ open class AndroidAppMain: Application {
 open class MainActivity: AppCompatActivity {
     constructor() {
     }
-
+    
     override fun onCreate(savedInstanceState: android.os.Bundle?) {
         super.onCreate(savedInstanceState)
         logger.info("starting activity")
         UIApplication.launch(this)
         enableEdgeToEdge()
-
+        
         setContent {
             val saveableStateHolder = rememberSaveableStateHolder()
             saveableStateHolder.SaveableStateProvider(true) {
@@ -56,7 +62,7 @@ open class MainActivity: AppCompatActivity {
                 SideEffect { saveableStateHolder.removeState(true) }
             }
         }
-
+        
         // Example of requesting permissions on startup.
         // These must match the permissions in the AndroidManifest.xml file.
         //let permissions = listOf(
@@ -68,51 +74,72 @@ open class MainActivity: AppCompatActivity {
         //let requestTag = 1
         //ActivityCompat.requestPermissions(self, permissions.toTypedArray(), requestTag)
     }
-
+    
     override fun onSaveInstanceState(outState: android.os.Bundle): Unit = super.onSaveInstanceState(outState)
-
+    
     override fun onRestoreInstanceState(bundle: android.os.Bundle) {
         // Usually you restore your state in onCreate(). It is possible to restore it in onRestoreInstanceState() as well, but not very common. (onRestoreInstanceState() is called after onStart(), whereas onCreate() is called before onStart().
         logger.info("onRestoreInstanceState")
         super.onRestoreInstanceState(bundle)
     }
-
+    
     override fun onRestart() {
         logger.info("onRestart")
         super.onRestart()
     }
-
+    
     override fun onStart() {
         logger.info("onStart")
         super.onStart()
     }
-
+    
     override fun onResume() {
         logger.info("onResume")
         super.onResume()
     }
-
+    
     override fun onPause() {
         logger.info("onPause")
         super.onPause()
     }
-
+    
     override fun onStop() {
         logger.info("onStop")
         super.onStop()
     }
-
+    
     override fun onDestroy() {
         logger.info("onDestroy")
         super.onDestroy()
     }
-
+    
     override fun onRequestPermissionsResult(requestCode: Int, permissions: kotlin.Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         logger.info("onRequestPermissionsResult: ${requestCode}")
     }
-
+    
     companion object {
+    }
+}
+
+@Composable
+internal fun SyncSystemBarsWithTheme() {
+    val dark = MaterialTheme.colorScheme.background.luminance() < 0.5f
+    
+    val transparent = AndroidColor.TRANSPARENT
+    val style = if (dark) {
+        SystemBarStyle.dark(transparent)
+    } else {
+        SystemBarStyle.light(transparent, transparent)
+    }
+    
+    val activity = LocalContext.current as? ComponentActivity
+    DisposableEffect(style) {
+        activity?.enableEdgeToEdge(
+            statusBarStyle = style,
+            navigationBarStyle = style
+        )
+        onDispose { }
     }
 }
 
@@ -120,6 +147,8 @@ open class MainActivity: AppCompatActivity {
 internal fun PresentationRootView(context: ComposeContext) {
     val colorScheme = if (isSystemInDarkTheme()) ColorScheme.dark else ColorScheme.light
     PresentationRoot(defaultColorScheme = colorScheme, context = context) { ctx ->
+        SyncSystemBarsWithTheme()
+        
         val contentContext = ctx.content()
         Box(modifier = ctx.modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
             RootView().Compose(context = contentContext)
